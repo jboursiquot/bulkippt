76857dfa5e9b45c3ada4280230d5b19421dd1b6a
Johnny Boursiquot <jboursiquot@gmail.com>
2012-11-14 01:39:46 -0500

    Improves code readability without changing interface

--- lib/bulkippt.rb
+++ lib/bulkippt.rb
@@ -29,40 +29,20 @@ module Bulkippt
 
     def extract_bookmarks(file_path)
       begin
-        raise Exception, "CSV not found: #{file_path}" if !File.exists?(file_path)
-
+        validate_file_path file_path
         @csv = CSV.read(file_path, {headers: true})
-        headers = @csv.headers.map(&:downcase) #normalize colum titles
-
-        #url and title are required, description and folder are not
-        raise Exception, "Missing 'url' column" unless headers.include? 'url'
-        raise Exception, "Missing 'title' column" unless headers.include? 'title'
-
-        bookmarks = []
-        @csv.each do |b|
-          row = {}
-          b.to_hash.each_pair do |k,v|
-            row.merge!({k.downcase => v}) 
-          end
-          bookmark = OpenStruct.new(url: row['url'], title: row['title'])
-          bookmark.description = row['description'] if headers.include? 'description'
-          bookmark.folder = row['folder'] if headers.include? 'folder'
-          bookmarks << bookmark
-        end
-        bookmarks
+        validate_headers(normalize_headers(@csv.headers))
+        parse_csv(@csv)
       rescue => e
         @logger.error e.message
       end
     end
 
     def submit_bookmarks(bookmarks)
-      submitted = []
       begin
+        submitted = []
         bookmarks.each do |bookmark|
-          clip = @service.clips.build
-          clip.url = bookmark.url
-          clip.title = bookmark.title
-          clip.notes = bookmark.description
+          clip = clip_from_bookmark bookmark
           if clip.save
             submitted << clip
             @logger.info "SUCCESS: #{clip.title} #{clip.url}"
@@ -74,6 +54,44 @@ module Bulkippt
       rescue => e
         @logger.error e.message
       end
+    end
+
+    private
+
+    def parse_csv(csv)
+      result = []
+      csv.each do |b|
+        row = {}
+        b.to_hash.each_pair {|k,v| row.merge!({k.downcase => v})}
+        result << bookmark_instance(row['url'], row['title'], row.fetch('description',''), row.fetch('folder',''))
+      end
+      result
+    end
+
+    def normalize_headers(headers)
+      headers.map(&:downcase)
+    end
+
+    def validate_file_path(file_path)
+      raise Exception, "CSV not found: #{file_path}" if !File.exists?(file_path)
+    end
+
+    def validate_headers(headers)
+      #url and title are required, description and folder are not
+      raise Exception, "Missing 'url' column" unless headers.include? 'url'
+      raise Exception, "Missing 'title' column" unless headers.include? 'title'
+    end
+
+    def bookmark_instance(url, title, desc, folder)
+      OpenStruct.new(url: url, title: title, description: desc, folder: folder)
+    end
+
+    def clip_from_bookmark(bookmark)
+      clip = @service.clips.build
+      clip.url = bookmark.url
+      clip.title = bookmark.title
+      clip.notes = bookmark.description
+      clip
     end
 
   end
